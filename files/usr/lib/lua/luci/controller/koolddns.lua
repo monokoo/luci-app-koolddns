6ea57a128933bbd523a526614609f99823b0d1bc
monokoo <realstones2012@gmail.com>
2018-01-29 17:48:37 +0800

    fix issue

--- files/usr/lib/lua/luci/controller/koolddns.lua
+++ files/usr/lib/lua/luci/controller/koolddns.lua
@@ -12,7 +12,7 @@ function act_nslookup()
 local e={}
 e.index=luci.http.formvalue("index")
 --e.value=luci.sys.exec("nslookup %q localhost 2>&1|grep 'Address 1:'|tail -n1|cut -d' ' -f3"%luci.http.formvalue("domain"))
-e.value=luci.sys.exec("dig %q 2>&1 |grep 'IN'|awk '{print $5}'|grep -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}'|head -n1"%luci.http.formvalue("domain"))
+e.value=luci.sys.exec("dig @8.8.4.4 %q 2>&1 |grep 'IN'|awk '{print $5}'|grep -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}'|head -n1"%luci.http.formvalue("domain"))
 if e.value=="" then
 e.value=luci.sys.exec("dig @8.8.8.8 %q 2>&1 |grep 'IN'|awk '{print $5}'|grep -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}'|head -n1"%luci.http.formvalue("domain"))
 if e.value=="" then
